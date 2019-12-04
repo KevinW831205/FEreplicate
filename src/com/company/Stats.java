c67b213a0156551de0a103e46ed166b0d7196b85
Kevin Wang <bird831205@hotmail.com>
2019-12-04 10:59:12 -0500

    some fields int stats

--- src/com/company/Stats.java
+++ src/com/company/Stats.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Stats {
+
+    private int level;
+    private int healthPoint;
+    private int strength;
+    private int magic;
+    private int dexterity;
+    private int speed;
+    private int luck;
+    private int defense;
+    private int resistance;
+    private int charisma;
+    private int movement;
+
+
+
 }
