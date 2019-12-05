0b8d429db911824f472fff78e62119622b9c2c39
KevinW <bird831205@hotmail.com>
2019-12-05 08:30:47 -0500

    setup of character class

--- src/main/java/com/github/kevinw831205/Character/FECharacter.java
+++ src/main/java/com/github/kevinw831205/Character/FECharacter.java
@@ -1,8 +1,14 @@
 package com.github.kevinw831205.Character;
 
+import java.util.ArrayList;
+
 public class FECharacter {
     private String name;
-
+    private CharacterClass cClass;
+    private Stats stats;
+    private Inventory inventory;
+    private ArrayList<Object> changeableClass;
+    
 
     public FECharacter(String name) {
         this.name = name;
