a8fc503415128d79d27a616c0a8aee76f17c906b
KevinW <bird831205@hotmail.com>
2019-12-11 08:26:13 -0500

    defining an item interface

--- src/main/java/com/github/kevinw831205/Character/CharacterClass/UniqueClass/Main.java
+++ src/main/java/com/github/kevinw831205/Character/CharacterClass/UniqueClass/Main.java
@@ -2,7 +2,7 @@ package com.github.kevinw831205.Character.CharacterClass.UniqueClass;
 
 public class Main {
     public static void main(String[] args) {
-        Commoner commoner = new CommonerBuilder()
-                .build();
+        Commoner commoner = new Commoner();
+        System.out.println(commoner.getClassName());
     }
 }
