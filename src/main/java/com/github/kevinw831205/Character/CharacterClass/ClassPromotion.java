dab5d21f548913f659e7ada7b70a787a9e65af38
KevinW <bird831205@hotmail.com>
2019-12-28 17:21:36 +0800

    initial promotion consideration

--- src/main/java/com/github/kevinw831205/Character/CharacterClass/ClassPromotion.java
+++ src/main/java/com/github/kevinw831205/Character/CharacterClass/ClassPromotion.java
@@ -1,4 +1,7 @@
 package com.github.kevinw831205.Character.CharacterClass;
 
 public interface ClassPromotion {
+    boolean haveWeaponSkill();
+    boolean meetMinLevel();
+    boolean canPromote();
 }
