246db4c2c42eda96f152ccd757c2c2feb5adda12
Kevin Wang <bird831205@hotmail.com>
2020-01-08 14:46:03 -0500

    test dances

---
+++ src/test/java/com/github/kevinw831205/Character/AbilitiesTest/DanceTest.java
@@ -0,0 +1,11 @@
+package com.github.kevinw831205.Character.AbilitiesTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DanceTest {
+    @Test
+    public void notImplemented(){
+        Assert.assertTrue(false);
+    }
+}
