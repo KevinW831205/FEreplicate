88ff9f91baa34ff6acd645b3cbb88737d201d3e7
Kevin Wang <bird831205@hotmail.com>
2020-01-14 09:13:18 -0500

    equipper interface

--- src/main/java/com/github/kevinw831205/Character/Equip/Equipper.java
+++ src/main/java/com/github/kevinw831205/Character/Equip/Equipper.java
@@ -1,5 +1,7 @@
 package com.github.kevinw831205.Character.Equip;
 
 public interface Equipper<someEquippable extends Equippable> {
+    public someEquippable equip(Equippable toEquip, int slot);
+    public someEquippable unEquip(Equippable toUnEquip, int slot);
 
 }
